968bad569eb665bdc87ae05defb8ede7a079aefe
Matheusmno <mnomatheus@gmail.com>
2023-06-07 14:21:31 +0200

    fixed notch filter bug

--- filters.py
+++ filters.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.signal import butter, filtfilt
+from scipy.signal import butter, filtfilt, iirnotch
 
 
 def apply_butterworth(signal:np.ndarray, fs:float, f_type:str="high", cutoff:int=15, order:int=3) -> np.ndarray:
@@ -9,9 +9,11 @@ def apply_butterworth(signal:np.ndarray, fs:float, f_type:str="high", cutoff:int
     
     return np.array(filtfilt(b, a, signal))
 
-def apply_notch_filter(signal:np.ndarray, notch_freq: float, fs: float) -> np.ndarray:
-    nyquist = 0.5 * fs
-    notch_normalized = notch_freq / nyquist
-    b, a = butter(2, [notch_normalized - 0.02, notch_normalized + 0.02], btype='bandstop', analog=False)
+def apply_notch(signal: np.ndarray, fs: float, notch_freq: float=50.0, Q: int=15) -> np.ndarray:
+    # Create/view notch filter
+    b_notch, a_notch = iirnotch(notch_freq, Q, fs)
+
+    # Apply notch filter to signal
+    notched_signal = filtfilt(b_notch, a_notch, signal)
     
-    return np.array(filtfilt(b, a, signal))
+    return np.array(notched_signal)
